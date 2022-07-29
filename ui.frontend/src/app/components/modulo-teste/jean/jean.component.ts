2b21b55c8051c058737f432d4db44270ef2545f5
Jean Santos <jean.costa@portoseguro.com.br>
2022-07-29 04:37:46 -0300

    ✨ Consertando Dialog e TS

--- ui.frontend/src/app/components/modulo-teste/jean/jean.component.ts
+++ ui.frontend/src/app/components/modulo-teste/jean/jean.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JeanComponent implements OnInit {
 
-  public texto: string = 'texto';
+  public texto = 'texto';
 
   constructor() { }
 
