a66d0cf66a8992e122e40fd6337da0b1ce2ef792
Jean Santos <jean.costa@portoseguro.com.br>
2022-11-29 21:52:51 -0300

    Add lombok - Component Header - App Fly

--- core/src/main/java/com/aem/sites/developer/core/models/projects/app_fly/AppFlyHeader.java
+++ core/src/main/java/com/aem/sites/developer/core/models/projects/app_fly/AppFlyHeader.java
@@ -2,6 +2,7 @@ package com.aem.sites.developer.core.models.projects.app_fly;
 
 import java.util.List;
 import javax.inject.Inject;
+import lombok.Getter;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 import com.adobe.cq.export.json.ComponentExporter;
@@ -10,8 +11,10 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
+import static lombok.AccessLevel.NONE;
 import static org.apache.sling.models.annotations.DefaultInjectionStrategy.OPTIONAL;
 
+@Getter
 @Model( adaptables = SlingHttpServletRequest.class,
         adapters = { ComponentExporter.class, AppFlyHeader.class, },
 		resourceType = AppFlyHeader.RESOURCE_TYPE,
@@ -21,29 +24,18 @@ public class AppFlyHeader implements ComponentExporter {
 
 	static final String RESOURCE_TYPE = "aem-sites-developer/components/projects/app-fly/app-fly-header";
 
+	@Getter(NONE)
 	@Inject
     ResourceResolver resourceResolver;
 
 	@ValueMapValue
 	private String logo;
 
-	@Inject
+	@ValueMapValue
 	private String background;
 
 	@ChildResource
 	List<Resource> options;
-
-	public String getLogo() {
-		return logo;
-	}
-
-	public String getBackground() {
-		return background;
-	}
-
-	public List<Resource> getOptions() {
-		return options;
-	}
 
 	@Override
 	public String getExportedType() {
